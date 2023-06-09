62c77074cfdd2853ab1547855f811fd97898bd2a
abhishek-sonawane <abhisheksonwane57@gmail.com>
2023-06-09 16:14:49 +0530

    style changes

--- client/src/Components/Card.jsx
+++ client/src/Components/Card.jsx
@@ -4,7 +4,7 @@ function Card({details}) {
   return (
     <div className='p-7 border text-left rounded-xl'>
       <p className=' text-2xl py-7'>{details.title}</p>
-      <p className=' max-w-xl'>{details.description}</p>
+      <p className=' max-w-xl  text-gray-500 font-medium'>{details.description}</p>
     </div>
   )
 }
--- client/src/Components/Feed.jsx
+++ client/src/Components/Feed.jsx
@@ -4,7 +4,8 @@ import Card from './Card'
 function Feed({data}) {
   return (
     <div>
-    <p className=' font-mono font-bold text-4xl text-red-400' >Angel co</p>
+    <p className=' font-mono font-bold text-4xl text-red-400 ' >Angel co</p>
+    
    <div className='flex flex-col gap-7 p-7'>
    {data.length>0&&data.map(item=>{
       return <Card details = {item} />
