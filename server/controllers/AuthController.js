752f0ec88cc0310d9ed7fb7c97d70baf9f4096a8
abhishek-sonawane <abhisheksonwane57@gmail.com>
2023-10-23 15:38:11 +0530

    moved login and register to auth route

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -35,4 +35,23 @@ const loginUser = async (req, res) => {
   }
 }
 
-module.exports = loginUser
+const registerUser = async(req,res)=>{
+  try {
+      const {username,password} = req.body
+      console.log(username, password)
+  if(username && password){
+      console.log('working')
+      const user = new Users({
+          username,
+         password
+      })
+      await user.save()
+     return res.status(200).send('user saved successfully')
+  }
+ return res.status(403).send('please submit all the details')
+  } catch (error) {
+      console.log(error.message)
+          res.status(403).send('something went wrong')
+  }
+}
+module.exports = {loginUser,registerUser}
--- server/routes/user.js
+++ server/routes/user.js
@@ -10,24 +10,5 @@ router.get('/user/:id', auth,getUserDetails)
 
 router.post('/user/logout',auth,postLogoutUser)
 
-router.post('/user/register',async(req,res)=>{
-    try {
-        const {username,password} = req.body
-        console.log(username, password)
-    if(username && password){
-        console.log('working')
-        const user = new Users({
-            username,
-           password
-        })
-        await user.save()
-       return res.status(200).send('user saved successfully')
-    }
-   return res.status(403).send('please submit all the details')
-    } catch (error) {
-        console.log(error.message)
-            res.status(403).send('something went wrong')
-    }
-})
 
 module.exports = router
