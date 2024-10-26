8729770e3ace182a81043965c4689dbe64915e73
abhishek-sonawane <abhisheksonwane57@gmail.com>
2024-10-26 18:43:48 +0530

    typescript implementation init

--- client/src/Components/ApplicationCard.tsx
+++ client/src/Components/ApplicationCard.tsx
@@ -1,33 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
-import { Document, Page, pdfjs } from "react-pdf";
-import { Link } from "react-router-dom";
+import { pdfjs } from "react-pdf";
 import { changeApplicationStatus } from "../services/APIcalls/jobs";
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
-function ApplicationCard({ item }) {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-  const [file, setFile] = useState();
+interface ApplicationCardProps {
+  item: {
+    _id: string,
+    name: string,
+    email: string,
+    status: string,
+    createdAt: string,
+    updatedAt: string;
+    cvPDF: { path: string };
+  }
+}
+
+function ApplicationCard({ item }: ApplicationCardProps) {
+  const [pdfLink, setPdfLink] = useState<string | null>(null);
   console.log(item);
 
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
+  const applicationStatuses = ["applied", "resume_viewed"];
 
-  const viewPdfHandle = async (fileName) => {
-    const dat = await fetch(
-      `${import.meta.env.VITE_BACKEND_ENDPOINT}/file/${fileName}`
-    );
-    console.log(dat);
-    const result = await dat.json();
 
-    console.log(result);
-    return result;
+
+  const returnPdfPathLink = async (fileName: string) => {
+    const pdfPath = getPdfPath(fileName.replace(/\\/g, "/"))
+    return `${import.meta?.env?.VITE_BACKEND_ENDPOINT}/file/${pdfPath}`;
   };
 
-  const applicationStatuses = ["applied", "resume_viewed"];
-  const selectHandler = (e) => {
+  const selectHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
     console.log("e", e.target.value);
     const newStatus = e.target.value;
     const appStatusMethod = async () => {
@@ -37,10 +39,20 @@ function ApplicationCard({ item }) {
     appStatusMethod();
   };
 
-  const getPdfPath = (item) => {
+  const getPdfPath = (item: string) => {
     console.log(item.split("/")[2]);
     return item.split("/")[2];
   };
+
+  useEffect(() => {
+    const fetchPdfLink = async () => {
+      const link = await returnPdfPathLink(item?.cvPDF?.path);
+      setPdfLink(link);
+    };
+    fetchPdfLink();
+  }, [item?.cvPDF?.path]);
+
+
   return (
     <div className=" border border-slate-300 p-10 w-full max-w-md">
       <div className="flex flex-row items-center gap-2">
@@ -66,24 +78,22 @@ function ApplicationCard({ item }) {
           applicationStatuses.map((ApplicationItem) => {
             if (ApplicationItem == item.status) {
               return (
-                <option selected value={ApplicationItem}>
+                <option key={ApplicationItem} selected value={ApplicationItem}>
                   {ApplicationItem}
                 </option>
               );
             }
-            return <option value={ApplicationItem}>{ApplicationItem}</option>;
+            return <option key={ApplicationItem} value={ApplicationItem}>{ApplicationItem}</option>;
           })}
       </select>
       <button className="px-5 text-white rounded-lg py-3 flex bg-red-400 font-bold">
-        <a
-          target="_blank"
-          href={`${import.meta.env.VITE_BACKEND_ENDPOINT}/file/${getPdfPath(
-            item.cvPDF.path.replace(/\\/g, "/")
-          )}`}
-        >
-          {" "}
-          view Resume
-        </a>
+        {pdfLink ? (
+          <a target="_blank" href={pdfLink} rel="noreferrer">
+            View Resume
+          </a>
+        ) : (
+          "Loading..."
+        )}
       </button>
     </div>
   );
